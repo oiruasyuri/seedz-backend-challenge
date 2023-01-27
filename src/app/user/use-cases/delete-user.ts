a7f10efe49af7647c77c45b02e1c5ea5b1fbaa47
Yuri Ruas <yuri.sdor@gmail.com>
2023-01-27 02:57:42 -0300

    feat(user): create business rule for method delete user

--- src/app/user/use-cases/delete-user.ts
+++ src/app/user/use-cases/delete-user.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../repositories/user-repository';
+import { UserNotFound } from './errors/user-not-found';
 
 @Injectable()
 export class DeleteUser {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(id: string): Promise<void> {}
+  async execute(id: string): Promise<void> {
+    const userExists = await this.userRepository.findById(id);
+
+    if (!userExists) throw new UserNotFound();
+
+    await this.userRepository.delete(id);
+  }
 }
