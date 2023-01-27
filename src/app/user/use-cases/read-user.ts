2b5847690347c9cbee55d584c7cb1fcace4b46a0
Yuri Ruas <yuri.sdor@gmail.com>
2023-01-27 01:11:21 -0300

    feat(user): create business rule for method read user

--- src/app/user/use-cases/read-user.ts
+++ src/app/user/use-cases/read-user.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { UserRepository } from '../repositories/user-repository';
+import { UserNotFound } from './errors/user-not-found';
 
 @Injectable()
 export class ReadUser {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute(id: string): Promise<User> {}
+  async execute(id: string): Promise<User> {
+    const user = await this.userRepository.findById(id);
+
+    if (!user) throw new UserNotFound();
+
+    return user;
+  }
 }
