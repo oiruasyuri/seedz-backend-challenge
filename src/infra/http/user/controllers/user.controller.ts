02db03da847a5c254f191284c995f0a0ed4ad772
Yuri Ruas <yuri.sdor@gmail.com>
2023-01-27 04:01:23 -0300

    feat(user): set the read, update and delete routes to private

--- src/infra/http/user/controllers/user.controller.ts
+++ src/infra/http/user/controllers/user.controller.ts
@@ -8,7 +8,9 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/app/auth/guards/jwt-auth.guard';
 import { CreateUserDTO } from 'src/app/user/dtos/create-user-dto';
 import { UpdateUserDTO } from 'src/app/user/dtos/update-user-dto';
 import { CreateUser } from 'src/app/user/use-cases/create-user';
@@ -32,18 +34,21 @@ export class UserController {
     await this.createUser.execute(createUser);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   @HttpCode(HttpStatus.OK)
   async read(@Param('id') id: string) {
     return UserViewModel.toHTTP(await this.readUser.execute(id));
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async update(@Param('id') id: string, @Body() updateUser: UpdateUserDTO) {
     await this.updateUser.execute(id, updateUser);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('id') id: string) {
