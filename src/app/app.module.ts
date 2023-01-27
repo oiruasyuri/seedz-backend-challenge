2665d8090a2b4a598c8d0665dcf3c748bb6e81ac
Yuri Ruas <yuri.sdor@gmail.com>
2023-01-26 23:27:29 -0300

    configure prisma and docker-compose for mysql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/infra/database/database.module';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+})
 export class AppModule {}
