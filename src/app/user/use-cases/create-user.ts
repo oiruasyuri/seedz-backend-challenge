eb513cc65cf2997e23ac53706d2d977b5433eed3
Yuri Ruas <yuri.sdor@gmail.com>
2023-01-27 00:46:20 -0300

    feat(user): create business rule for method create user

--- src/app/user/use-cases/create-user.ts
+++ src/app/user/use-cases/create-user.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
 import { CreateUserDTO } from '../dtos/create-user-dto';
 import { UserRepository } from '../repositories/user-repository';
+import { EmailInUse } from './errors/email-in-use';
+import { EmailIsRequired } from './errors/email-is-required';
+import { NameIsRequired } from './errors/name-is-required';
+import { PasswordIsRequired } from './errors/password-is-required';
 
 @Injectable()
 export class CreateUser {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async execute({ name, email, password }: CreateUserDTO): Promise<void> {}
+  async execute({ name, email, password }: CreateUserDTO): Promise<void> {
+    if (!name) throw new NameIsRequired();
+    if (!email) throw new EmailIsRequired();
+    if (!password) throw new PasswordIsRequired();
+
+    const emailInUse = await this.userRepository.findByEmail(email);
+
+    if (emailInUse) throw new EmailInUse();
+
+    const user = { id: randomUUID(), name, email, password };
+
+    await this.userRepository.create(user);
+  }
 }
